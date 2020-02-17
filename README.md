a075e5e85c45288f3ee8c2bc1911af131162ce27
Diogo Fernandes <diogo.fernandes@capgemini.com>
2020-02-17 01:14:43 +0000

    Add README.md and test for app-detail

---
+++ README.md
@@ -0,0 +1,44 @@
+# apps_listing v0.1
+
+Apps_listing v0.1 is an app that aims to display information about other apps as well as their subscriptions.
+
+The app can be broke into two major pieces which are the frontend, using Angular, and the backend, using Node.js.
+
+The frontend is responsible for displaying all the information that retrieve from the services. The backend is responsible to get all the data from the data source, and expose the services to the frontend.
+
+## Installation
+
+Use the package manager [npm](https://www.npmjs.com/) to install all the packages needed to run apps_listing v0.1.
+
+```bash
+npm install
+```
+
+## Usage
+
+Firstly start the Node.js server with which the Angular app will comunicate.
+
+```bash
+cd node-apps-list
+npm run dev
+```
+
+After that serve the Angular app to open a browser instance with the app running.
+
+```bash
+cd angular-apps-list
+npm run dev
+```
+
+## Testing
+<>
+In order to run unit tests developed for the app run the following command
+
+```
+cd angular-apps-list
+ng test
+```
+
+## Authors
+Diogo Fernandes
+
--- angular-apps-list/src/app/shared/app-detail/app-detail.component.spec.ts
+++ angular-apps-list/src/app/shared/app-detail/app-detail.component.spec.ts
@@ -3,6 +3,9 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppDetailComponent } from './app-detail.component';
 import { AppMaterialModule } from 'src/app/app-material/app-material.module';
 import { App } from 'src/app/models/App';
+import { Component } from '@angular/core';
+import { Subscription } from 'src/app/models/Subscription';
+import { By } from '@angular/platform-browser';
 
 describe('AppDetailComponent', () => {
   let component: AppDetailComponent;
@@ -22,11 +25,49 @@ describe('AppDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppDetailComponent);
     component = fixture.componentInstance;
-    component.app = new App();
+    component.app = new App().deserialize({
+      name: '',
+      description: '',
+      categories: [],
+      subscriptions: []
+    });
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show app name', () => {
+    const appObj = {
+      name: 'AppTest',
+      description: 'App for testing component',
+      categories: ['Test'],
+      subscriptions: []
+    };
+    const newApp = new App().deserialize(appObj);
+    component.app = newApp;
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('mat-card-title').innerText).toContain(appObj.name);
+  });
+
+  it('should have same number of subscriptions', () => {
+    const arraySubs = [];
+
+    for (let i = 0; i <= 10; i++) {
+      arraySubs.push(new Subscription().deserialize({
+        name: 'test',
+        price: Math.floor(Math.random() * 1001)
+      }));
+    }
+
+    const appObj = {
+      name: 'AppTest',
+      description: 'App for testing component',
+      categories: ['Test'],
+      subscriptions: arraySubs
+    };
+    const newApp = new App().deserialize(appObj);
+    component.app = newApp;
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('.subscriptionItem')).length).toEqual(arraySubs.length);
   });
 });
